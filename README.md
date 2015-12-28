78e404c1228078890b4cdfd97f70ed0c11f12aeb
drJeckyll <drJeckyll@Jeckyll.net>
2015-12-28 22:32:11 +0200

    * Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # esp-mqtt-http-ota-ir-ac-chunlan
-This is same is esp-mqtt-http-ir-ac-chunlan but with ota update
+
+This is same as https://github.com/drJeckyll/esp-mqtt-http-ir-ac-chunlan, but was ported to use rBoot and have OTA updates.
+
+There also two new topics /office/service/ac/restart and /office/service/ac/update. If you send something to 'restart' esp8266 will restart. If you send something to 'update' OTA update will be performed.
+
+Settings for WIFI, MQTT and OTA are in Makefile, or you can set them via env:
+
+```DEVICE=test WIFI_SSID=Test make```
+
+You must set DEVICE since it is used both OTA and MQTT.
+
+If you want to use this project unmodified you must have at least 16Mbit flash (2MB).
+
+
+You can read more for this project here (Bulgarian): http://f-e-n.net/ur
+
+In my office I have some cheap AC named Chunlan which I want to operate remotly. So I make this IR remote control with ESP8266. It works via MQTT and also have WEB interafce.
+
+Settings for MQTT and WIFI are in include/user_config.h.
+
+IR protocol is like this:
+```
+ 0 \ 11 Heat | 00 Cool
+ 1 / 10 Dry
+ 2
+ 3 - 1 On | 0 Off
+ 4
+ 5
+ 6
+ 7
+
+ 8 | Temp 9
+ 9 |
+10 |
+11 |
+12 |
+13 |
+14
+15
+
+16 \ 00 Fan auto | 01 Fan 2
+17 / 11 Fan 1    | 10 Fan 3
+18
+19 - Vane
+20
+21
+22
+23
+```
+
+Temp = Temp + 9
+
+I try to use PWM, but I failed, so I make it with delays. You can see code in user/ac.c - ir_send().
+
+You can use the following template and just set bits which you want:
+000000000000000000000000001001001110000000000000000000000000000000110000
+
+You can change settings via JSON and MQTT on topic /office/service/ac. And on /office/service/ac/settings you will receive new settings:
+{"power":"on","mode":"heat","temp":"18","fan":"3","swing":"off","dht_temp":"2000","dht_humid":"3900"}
+
+You can get settings via JSON:
+curl -u user:pass http://ip.address/load.tpl
+{"power":"on","mode":"heat","temp":"18","fan":"3","swing":"off","dht_temp":"2000","dht_humid":"3900"}
+
+You can set settings:
+curl -u user:pass http://ip.address/save.cgi?power=on&mode=heat
+OK
+
+Valid settings are:
+power: on/off
+mode: heat/cool
+temp: 15-30
+fan: 1/2/3/auto
+swing: on/off
+
+This code was tested with ESP SDK 0.9.3.
+
+Schematics is something like: http://alexba.in/blog/2013/06/08/open-source-universal-remote-parts-and-pictures/
---
+++ main.c
@@ -0,0 +1,221 @@
+#include <c_types.h>
+#include <ets_sys.h>
+#include <osapi.h>
+#include <gpio.h>
+#include <os_type.h>
+#include <user_interface.h>
+#include <mem.h>
+
+#include "main.h"
+#include "config.h"
+#include "uart.h"
+#include "wifi.h"
+#include "mqtt.h"
+#include "pwm.h"
+#include "ota.h"
+#include "debug.h"
+
+#include "ac.h"
+#include "dht22.h"
+
+#include "httpd.h"
+#include "auth.h"
+#include "cgi.h"
+#include "httpdespfs.h"
+
+MQTT_Client mqttClient;
+
+void ICACHE_FLASH_ATTR wifiConnectCb(uint8_t status)
+{
+	if(status == STATION_GOT_IP){
+		MQTT_Connect(&mqttClient);
+	} else {
+		MQTT_Disconnect(&mqttClient);
+	}
+}
+
+void ICACHE_FLASH_ATTR mqttConnectedCb(uint32_t *args)
+{
+	MQTT_Client* client = (MQTT_Client*)args;
+	INFO("MQTT: Connected\r\n");
+
+	MQTT_Subscribe(client, MQTT_TOPIC_AC, 0);
+	MQTT_Subscribe(client, MQTT_TOPIC_SETTINGS, 0);
+	MQTT_Subscribe(client, MQTT_TOPIC_UPDATE, 0);
+	MQTT_Subscribe(client, MQTT_TOPIC_RESTART, 0);
+}
+
+void ICACHE_FLASH_ATTR mqttDisconnectedCb(uint32_t *args)
+{
+	MQTT_Client* client = (MQTT_Client*)args;
+	INFO("MQTT: Disconnected\r\n");
+}
+
+void ICACHE_FLASH_ATTR mqttPublishedCb(uint32_t *args)
+{
+	MQTT_Client* client = (MQTT_Client*)args;
+	INFO("MQTT: Published\r\n");
+}
+
+void ICACHE_FLASH_ATTR mqttPublishSettings(temp_humid) {
+	char buff[200] = "";
+	
+	ac_get_settings(buff, temp_humid);
+	MQTT_Publish(&mqttClient, MQTT_TOPIC_SETTINGS, buff, os_strlen(buff), 0, 0);
+	INFO("MQTT send topic: %s, data: %s \r\n", MQTT_TOPIC_SETTINGS, buff);
+}
+
+void ICACHE_FLASH_ATTR mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const char *data, uint32_t data_len)
+{
+	char *topicBuf = (char*)os_zalloc(topic_len + 1),
+	     *dataBuf  = (char*)os_zalloc(data_len + 1);
+
+	MQTT_Client* client = (MQTT_Client*)args;
+
+	os_memcpy(topicBuf, topic, topic_len);
+	topicBuf[topic_len] = 0;
+
+	os_memcpy(dataBuf, data, data_len);
+	dataBuf[data_len] = 0;
+
+	INFO("Receive topic: %s, data: %s\r\n", topicBuf, dataBuf);
+
+	if (os_strcmp(topicBuf, MQTT_TOPIC_AC) == 0)
+	{
+		if (os_strcmp(dataBuf, "settings") == 0) {
+			mqttPublishSettings(1);
+		} else {
+			// try to parse and set
+			// shift 1 byte left
+			char *token, *endtoken;
+			int t, apply = 0;
+			char cmd[20], val[20];
+			for (t = 1; t < os_strlen(dataBuf); t++) dataBuf[t - 1] = dataBuf[t];
+			dataBuf[os_strlen(dataBuf) - 2] = '\0';
+			token = strtok_r(dataBuf, ",", &endtoken);
+			while (token != NULL) {
+				int i = 0;
+				char *tmp, *endtmp;
+				//INFO("> %s\n", token);
+				tmp = strtok_r(token, ":", &endtmp);
+				while (tmp != NULL) {
+					//INFO(">> %s\n", tmp);
+					// trim "
+					for (t = 1; t < os_strlen(tmp); t++) tmp[t - 1] = tmp[t];
+					tmp[os_strlen(tmp) - 2] = '\0';
+					if (i == 0) os_strcpy(cmd, tmp);
+					if (i == 1) os_strcpy(val, tmp);
+
+					tmp = strtok_r(NULL, ":", &endtmp);
+					i++;
+				}
+				//INFO(">>> %s: %s\n", cmd, val);
+				
+				if (os_strcmp(cmd, "power") == 0) {
+					apply = 1;
+					ac_set_power(val);	
+				} else
+				if (os_strcmp(cmd, "mode") == 0) {
+					apply = 1;
+					ac_set_mode(val);
+				} else
+				if (os_strcmp(cmd, "temp") == 0) {
+					apply = 1;
+					ac_set_temp(val);
+				} else
+				if (os_strcmp(cmd, "fan") == 0) {
+					apply = 1;
+					ac_set_fan(val);
+				} else
+				if (os_strcmp(cmd, "swing") == 0) {
+					apply = 1;
+					ac_set_swing(val);
+				}
+
+				token = strtok_r(NULL, ",", &endtoken);
+			}
+			if (apply) {
+				ir_send();
+				mqttPublishSettings(0);
+				CFG_Save();
+			}
+		}
+	} else
+	if (os_strcmp(topicBuf, MQTT_TOPIC_UPDATE) == 0)
+	{
+		OtaUpdate();
+	} else
+	if (os_strcmp(topicBuf, MQTT_TOPIC_RESTART) == 0)
+	{
+		system_restart();
+	}
+}
+
+int ICACHE_FLASH_ATTR myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pass, int passLen)
+{
+	if (no == 0)
+	{
+		os_strcpy(user, MQTT_USER);
+		os_strcpy(pass, MQTT_PASS);
+
+		return 1;
+
+		//Add more users this way. Check against incrementing no for each user added.
+		//	} else if (no==1) {
+		//		os_strcpy(user, "user1");
+		//		os_strcpy(pass, "something");
+		//		return 1;
+	}
+
+	return 0;
+}
+
+HttpdBuiltInUrl builtInUrls[]={
+	{"/*", authBasic, myPassFn},
+	{"/", cgiRedirect, "/index.html"},
+	{"/save.cgi", saveCGI, NULL},
+	{"/settings.cgi", settingsCGI, NULL},
+	//{"/load.tpl", cgiEspFsTemplate, loadTpl},
+	{"*", cgiEspFsHook, NULL}, //Catch-all cgi function for the filesystem
+	{NULL, NULL, NULL}
+};
+
+void ICACHE_FLASH_ATTR
+user_init()
+{
+	uart_init(BIT_RATE_115200, BIT_RATE_115200);
+	os_delay_us(1000000);
+
+	INFO("Starting up...\r\n");
+
+	INFO("Loading config...\r\n");
+	CFG_Load();
+
+	gpio_init();
+
+	INFO("Initializing MQTT...\r\n");
+	MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, sysCfg.security);
+	MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
+	MQTT_OnConnected(&mqttClient, mqttConnectedCb);
+	MQTT_OnDisconnected(&mqttClient, mqttDisconnectedCb);
+	MQTT_OnPublished(&mqttClient, mqttPublishedCb);
+	MQTT_OnData(&mqttClient, mqttDataCb);
+
+	INFO("Connect to WIFI...\r\n");
+	WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
+
+	// ir
+	INFO("Setup IR...\r\n");
+	ac_init();
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4);
+	gpio_output_set(0, BIT4, BIT4, 0);
+
+	// web
+	INFO("Setup WEB...\r\n");
+	//stdoutInit();
+	ioInit();
+	DHTInit();
+	httpdInit(builtInUrls, 80);
+
+	INFO("Startup completed. Now running from rom %d...\r\n", rboot_get_current_rom());
+}
